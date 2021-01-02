c22c75a5fd8bb13f448cd76c153d1317bf2e318d
Aivars <aivars.meijers@gmail.com>
2021-01-02 22:31:46 +0200

    Base views added

---
+++ Shared/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Aivars Meijers on 02/01/2021.
+//
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
