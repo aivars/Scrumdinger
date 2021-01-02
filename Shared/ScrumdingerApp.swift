d15389959e4c61f3242ea840b0f7e17358469aa2
Aivars <aivars.meijers@gmail.com>
2021-01-02 17:45:16 +0200

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Aivars Meijers on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
